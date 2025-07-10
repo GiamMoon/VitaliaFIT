afaab45c5ff7df3ee309d2f4ba81079366b4463f
Giampier De La Cruz <giampierdelacruz37@gmail.com>
2025-07-10 16:45:19 -0500

    Configuracion de backen para despliegue producciont

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -11,18 +12,22 @@ import { Exercise } from './exercises/entities/exercise.entity';
 import { Routine } from './routines/entities/routine.entity';
 import { WorkoutHistoryModule } from './workout-history/workout-history.module';
 import { WorkoutHistory } from './workout-history/entities/workout-history.entity';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }), // Importa y configura el módulo de configuración
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      // Usa la URL de la base de datos si existe (en Render), si no, usa la config local
-      url: process.env.DATABASE_URL, 
-      ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false, // Necesario para la conexión en Render
-      entities: [User, Exercise, Routine, WorkoutHistory],
-      synchronize: true, // Dejaremos esto en true por ahora para simplicidad
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        ssl: configService.get<string>('DATABASE_URL')
+          ? { rejectUnauthorized: false }
+          : false,
+        entities: [User, Exercise, Routine, WorkoutHistory],
+        synchronize: true,
+      }),
     }),
     UsersModule,
     AuthModule,
