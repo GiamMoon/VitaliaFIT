df2b4d2557c340f56d0e5dbed58d31b32eb9f117
Giampier De La Cruz <giampierdelacruz37@gmail.com>
2025-07-10 16:43:03 -0500

    Configuracion de backen para despliegue producciont

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,24 +11,24 @@ import { Exercise } from './exercises/entities/exercise.entity';
 import { Routine } from './routines/entities/routine.entity';
 import { WorkoutHistoryModule } from './workout-history/workout-history.module';
 import { WorkoutHistory } from './workout-history/entities/workout-history.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // Importa y configura el módulo de configuración
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'vitaliafit_db',
-      entities: [User, Exercise, Routine, WorkoutHistory], // Añadir WorkoutHistory
-      synchronize: true,
+      // Usa la URL de la base de datos si existe (en Render), si no, usa la config local
+      url: process.env.DATABASE_URL, 
+      ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false, // Necesario para la conexión en Render
+      entities: [User, Exercise, Routine, WorkoutHistory],
+      synchronize: true, // Dejaremos esto en true por ahora para simplicidad
     }),
     UsersModule,
     AuthModule,
     ExercisesModule,
     RoutinesModule,
-    WorkoutHistoryModule, // Añadir el módulo
+    WorkoutHistoryModule,
   ],
   controllers: [AppController],
   providers: [AppService],
