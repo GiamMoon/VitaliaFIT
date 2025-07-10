0bef2d2b90f35afbc72a3334acab2943ad4e8f12
Giampier De La Cruz <giampierdelacruz37@gmail.com>
2025-07-10 17:28:22 -0500

    Se habilita CORS

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common'; // Importar
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe()); // Añadir esta línea
+
+  // Habilita la validación global de datos
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Habilita CORS para permitir peticiones desde otros dominios
+  app.enableCors(); // <--- AÑADE ESTA LÍNEA
+
   await app.listen(3000);
 }
 bootstrap();
