ae09137e400e78ad32c724596d0a68021e46b910
Giampier De La Cruz <giampierdelacruz37@gmail.com>
2025-07-11 14:01:06 -0500

    Se agrega la actualizacion de rutinas

--- src/routines/routines.controller.ts
+++ src/routines/routines.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get, Post, Body, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, UseGuards } from '@nestjs/common';
 import { RoutinesService } from './routines.service';
 import { CreateRoutineDto } from './dto/create-routine.dto';
+import { UpdateRoutineDto } from './dto/update-routine.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'src/users/enums/role.enum';
 
 @Controller('routines')
+@UseGuards(AuthGuard('jwt'), RolesGuard) // Protegemos todo el controlador
 export class RoutinesController {
   constructor(private readonly routinesService: RoutinesService) {}
 
   @Post()
+  @Roles(Role.Admin)
   create(@Body() createRoutineDto: CreateRoutineDto) {
     return this.routinesService.create(createRoutineDto);
   }
@@ -16,8 +23,21 @@ export class RoutinesController {
     return this.routinesService.findAll();
   }
 
-  @Get(':id') // Ruta para obtener una rutina por su ID, ej: /routines/1
+  @Get(':id')
+  @Roles(Role.Admin)
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.routinesService.findOne(id);
+  }
+
+  @Patch(':id')
+  @Roles(Role.Admin)
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateRoutineDto: UpdateRoutineDto) {
+    return this.routinesService.update(id, updateRoutineDto);
+  }
+
+  @Delete(':id')
+  @Roles(Role.Admin)
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.routinesService.remove(id);
   }
 }
--- src/routines/routines.service.ts
+++ src/routines/routines.service.ts
@@ -1,9 +1,10 @@
-// CORRECCIÓN: Se añade NotFoundException
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateRoutineDto } from './dto/create-routine.dto';
+import { UpdateRoutineDto } from './dto/update-routine.dto';
 import { Routine } from './entities/routine.entity';
+import { Exercise } from 'src/exercises/entities/exercise.entity'; // Importar Exercise
 
 @Injectable()
 export class RoutinesService {
@@ -13,7 +14,7 @@ export class RoutinesService {
   ) {}
 
   create(createRoutineDto: CreateRoutineDto) {
-    const exercises = createRoutineDto.exerciseIds.map(id => ({ id }));
+    const exercises = createRoutineDto.exerciseIds.map(id => ({ id } as Exercise));
     const newRoutine = this.routineRepository.create({
       ...createRoutineDto,
       exercises,
@@ -22,20 +23,40 @@ export class RoutinesService {
   }
 
   findAll() {
-    return this.routineRepository.find();
+    // Incluimos la cuenta de ejercicios para mostrarla en la tabla
+    return this.routineRepository.find({ relations: ['exercises'] });
   }
 
-  // CORRECCIÓN: Método más robusto con manejo de errores explícito
   async findOne(id: number) {
     const routine = await this.routineRepository.findOne({
       where: { id },
-      relations: ['exercises'], // ¡Importante! Carga la relación
+      relations: ['exercises'],
+    });
+    if (!routine) {
+      throw new NotFoundException(`Routine with ID #${id} not found`);
+    }
+    return routine;
+  }
+
+  async update(id: number, updateRoutineDto: UpdateRoutineDto) {
+    // Usamos preload para cargar la rutina y fusionar los nuevos datos
+    const routine = await this.routineRepository.preload({
+      id: id,
+      ...updateRoutineDto,
+      // CORRECCIÓN: Mapeamos los IDs a objetos que TypeORM puede relacionar
+      exercises: updateRoutineDto.exerciseIds?.map(id => ({ id } as Exercise)),
     });
 
     if (!routine) {
       throw new NotFoundException(`Routine with ID #${id} not found`);
     }
 
-    return routine;
+    return this.routineRepository.save(routine);
+  }
+
+  async remove(id: number) {
+    const routine = await this.findOne(id);
+    await this.routineRepository.remove(routine);
+    return { message: `Routine with ID #${id} has been removed` };
   }
 }
