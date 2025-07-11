76ceaaab21defe8d2b91240b98a43929d4c1337b
Giampier De La Cruz <giampierdelacruz37@gmail.com>
2025-07-11 17:01:25 -0500

    CORS rutinas

--- src/routines/routines.controller.ts
+++ src/routines/routines.controller.ts
@@ -8,34 +8,39 @@ import { Roles } from 'src/auth/decorators/roles.decorator';
 import { Role } from 'src/users/enums/role.enum';
 
 @Controller('routines')
-@UseGuards(AuthGuard('jwt'), RolesGuard) // Protegemos todo el controlador
+// CORRECCIÓN: Eliminamos la protección a nivel de controlador de aquí
 export class RoutinesController {
   constructor(private readonly routinesService: RoutinesService) {}
 
-  @Post()
-  @Roles(Role.Admin)
-  create(@Body() createRoutineDto: CreateRoutineDto) {
-    return this.routinesService.create(createRoutineDto);
-  }
-
+  // Esta ruta ahora es pública, cualquiera puede ver las rutinas
   @Get()
   findAll() {
     return this.routinesService.findAll();
   }
 
-  @Get(':id')
+  // Las siguientes rutas SÍ necesitan protección
+  @Post()
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
   @Roles(Role.Admin)
+  create(@Body() createRoutineDto: CreateRoutineDto) {
+    return this.routinesService.create(createRoutineDto);
+  }
+
+  @Get(':id')
+  // Esta ruta también puede ser pública para que los usuarios vean los detalles
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.routinesService.findOne(id);
   }
 
   @Patch(':id')
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
   @Roles(Role.Admin)
   update(@Param('id', ParseIntPipe) id: number, @Body() updateRoutineDto: UpdateRoutineDto) {
     return this.routinesService.update(id, updateRoutineDto);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
   @Roles(Role.Admin)
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.routinesService.remove(id);
