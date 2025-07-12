a2edfcecd8b0df1898ce6a6709cf1be29aa2b29c
Giampier De La Cruz <giampierdelacruz37@gmail.com>
2025-07-12 13:36:35 -0500

    Se agrega recomendaciones de rutinas

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -1,17 +1,10 @@
-import { Module, forwardRef } from '@nestjs/common';
-import { RoutinesService } from '../routines/routines.service';
-import { RoutinesController } from '../routines/routines.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Routine } from '../routines/entities/routine.entity';
-import { ExercisesModule } from 'src/exercises/exercises.module';
+import { Module } from '@nestjs/common';
+import { ProfileController } from './profile.controller';
+import { UsersModule } from 'src/users/users.module';
+import { RoutinesModule } from 'src/routines/routines.module'; // Importar
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Routine]),
-    forwardRef(() => ExercisesModule),
-  ],
-  controllers: [RoutinesController],
-  providers: [RoutinesService],
-  exports: [RoutinesService], // <-- AÑADE ESTA LÍNEA
+  imports: [UsersModule, RoutinesModule], // Añadir RoutinesModule
+  controllers: [ProfileController],
 })
-export class RoutinesModule {}
+export class ProfileModule {}
--- src/routines/routines.module.ts
+++ src/routines/routines.module.ts
@@ -1,13 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { RoutinesService } from './routines.service';
 import { RoutinesController } from './routines.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Routine } from './entities/routine.entity';
+import { ExercisesModule } from 'src/exercises/exercises.module';
 
 @Module({
-  // Y aquí también:
-  imports: [TypeOrmModule.forFeature([Routine])],
+  imports: [
+    TypeOrmModule.forFeature([Routine]),
+    forwardRef(() => ExercisesModule),
+  ],
   controllers: [RoutinesController],
   providers: [RoutinesService],
+  exports: [RoutinesService], // <-- AÑADE ESTA LÍNEA
 })
 export class RoutinesModule {}
